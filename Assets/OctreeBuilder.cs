bf07002518a03781365c59e9b447bb494e7fe02c
Armin Rigo <arigo@tunes.org>
2020-04-05 20:31:25 +0200

    starting

---
+++ Assets/OctreeBuilder.cs
@@ -0,0 +1,234 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+
+//[ExecuteInEditMode]
+public class OctreeBuilder : MonoBehaviour
+{
+    public Light directionalLight;
+    public LayerMask cullingMask = -1;
+    public int gridResolution;
+    public float gridPixelSizeLevel0;
+    public int gridLevels;
+    public int computeBufferSize;
+    public Shader octreeBuilderShader;
+
+    public int[] _octree_dump;
+
+
+    Camera _shadowCam;
+    ComputeBuffer octree;
+
+
+    void OnDestroy()
+    {
+        if (octree != null)
+            octree.Release();
+    }
+
+
+    RenderTexture GetTemporaryTarget()
+    {
+        return RenderTexture.GetTemporary(gridResolution, gridResolution, 0,
+                                          RenderTextureFormat.R8);
+    }
+
+    const int OCTREE_ROOT = 8;
+
+    void MakeOctreeRecursively(List<int> nodes, int target, int levels)
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            nodes[target + i] = nodes.Count;
+            for (int j = 0; j < 8; j++)
+                nodes.Add(0);
+        }
+        levels--;
+        if (levels > 0)
+            for (int i = 0; i < 8; i++)
+                MakeOctreeRecursively(nodes, nodes[target + i], levels);
+    }
+
+    void BuildOctree()
+    {
+        if (octree != null)
+        {
+            octree.Release();
+            octree = null;
+        }
+        var cam = FetchShadowCamera();
+        var trackTransform = directionalLight.transform;
+        cam.transform.SetPositionAndRotation(trackTransform.position, trackTransform.rotation);
+
+        /* _VCT_WorldToOctreeMatrix: maps the cube in the directionalLight's orientation of size
+         *
+         *     2**gridLevels * gridPixelSizeLevel0 * gridResolution
+         *
+         * to a cube in [0,1]**3.  Note that the last rendered grid level is 'gridLevels - 1',
+         * so only the inner [0.25,0.75]**3 will ever be occupied.  That's what we want, with
+         * the outermost 0.25-sized cubes containing incoming radiance from the sky.
+         */
+        /*float octree_total_size = (1 << gridLevels) * gridResolution * gridPixelSizeLevel0;
+        Matrix4x4 world_to_octree_matrix = Matrix4x4.Scale(Vector3.one / octree_total_size) *
+                                           cam.transform.worldToLocalMatrix;
+        Shader.SetGlobalMatrix("_VCT_WorldToOctreeMatrix", world_to_octree_matrix);
+        Shader.SetGlobalInt("_VCT_GridResolution", gridResolution);*/
+
+
+        var initial_octree = new List<int>();
+        for (int j = 0; j < OCTREE_ROOT + 8; j++)
+            initial_octree.Add(0);
+        //MakeOctreeRecursively(initial_octree, OCTREE_ROOT, levels: 2);
+        octree = new ComputeBuffer(computeBufferSize, 4, ComputeBufferType.Counter);
+        octree.SetData(initial_octree);
+
+        var target = GetTemporaryTarget();
+        cam.targetTexture = target;
+
+        float global_scale = 0.5f / gridResolution;
+        int tree_levels = (int)Mathf.Log(gridResolution, 2);
+        int level = gridLevels - 1;
+        int orientation_mask = 7;
+
+        int MAX_STEPS = 5;
+
+        while (level >= 0)
+        {
+            float half_size = (1 << level) * gridResolution * gridPixelSizeLevel0 * 0.5f;
+            cam.orthographicSize = half_size;
+            cam.nearClipPlane = -half_size;
+            cam.farClipPlane = half_size;
+            /* in the shader: xyz *= _VCT_Scale.xxy;
+                              xyz += _VCT_Scale.zzz;
+               should turn the value (gridResolution/2, gridResolution/2, 1/2)
+               to (1/2, 1/2, 1/2)
+             */
+            Shader.SetGlobalVector("_VCT_Scale",
+                new Vector3(global_scale, global_scale * gridResolution,
+                            0.5f - gridResolution * 0.5f * global_scale));
+            Shader.SetGlobalInt("_VCT_TreeLevels", tree_levels);
+            Graphics.SetRandomWriteTarget(1, octree);
+
+            var orig_rotation = cam.transform.rotation;
+            var axis_y = cam.transform.up;
+            var axis_z = cam.transform.forward;
+
+            if ((orientation_mask & 1) != 0)
+            {
+                cam.RenderWithShader(octreeBuilderShader, "RenderType");
+            }
+            if ((orientation_mask & 2) != 0)
+            {
+                cam.transform.Rotate(axis_y, -90, Space.World);
+                cam.transform.Rotate(axis_z, -90, Space.World);
+                Shader.EnableKeyword("ORIENTATION_2");
+                cam.RenderWithShader(octreeBuilderShader, "RenderType");
+                Shader.DisableKeyword("ORIENTATION_2");
+                cam.transform.rotation = orig_rotation;
+            }
+            if ((orientation_mask & 4) != 0)
+            {
+                cam.transform.Rotate(axis_z, 90, Space.World);
+                cam.transform.Rotate(axis_y, 90, Space.World);
+                Shader.EnableKeyword("ORIENTATION_3");
+                cam.RenderWithShader(octreeBuilderShader, "RenderType");
+                Shader.DisableKeyword("ORIENTATION_3");
+                cam.transform.rotation = orig_rotation;
+            }
+
+            var flag = new int[3];
+            octree.GetData(flag, 0, 0, 3);
+            orientation_mask = 0;
+            orientation_mask |= flag[0] != 0 ? 1 : 0;
+            orientation_mask |= flag[1] != 0 ? 2 : 0;
+            orientation_mask |= flag[2] != 0 ? 4 : 0;
+            if (orientation_mask == 0)
+            {
+                tree_levels += 1;
+                level -= 1;
+                global_scale *= 0.5f;
+                orientation_mask = 7;
+            }
+            //_octree_dump = new int[592];
+            //octree.GetData(_octree_dump);
+            flag[0] = flag[1] = flag[2] = 0;
+            octree.SetData(flag, 0, 0, 3);
+
+
+            if (--MAX_STEPS == 0)
+                break;
+        }
+
+        cam.targetTexture = null;
+        RenderTexture.ReleaseTemporary(target);
+    }
+
+    Camera FetchShadowCamera()
+    {
+        if (_shadowCam == null)
+        {
+            // Create the shadow rendering camera
+            GameObject go = new GameObject("shadow cam (not saved)");
+            //go.hideFlags = HideFlags.HideAndDontSave;
+            go.hideFlags = HideFlags.DontSave;
+
+            _shadowCam = go.AddComponent<Camera>();
+            _shadowCam.orthographic = true;
+            _shadowCam.enabled = false;
+            _shadowCam.clearFlags = CameraClearFlags.Nothing;
+            _shadowCam.aspect = 1;
+            /* Obscure: if the main camera is stereo, then this one will be confused in
+             * the SetTargetBuffers() mode unless we force it to not be stereo */
+            _shadowCam.stereoTargetEye = StereoTargetEyeMask.None;
+        }
+        _shadowCam.cullingMask = cullingMask;
+        return _shadowCam;
+    }
+
+
+    void Update()
+    {
+        BuildOctree();
+    }
+
+    void OnDrawGizmos()
+    {
+        if (octree == null)
+            return;
+        _octree_dump = new int[5000];
+        octree.GetData(_octree_dump);
+
+        Gizmos.matrix = directionalLight.transform.localToWorldMatrix;
+
+
+        void DrawRec(int index, Vector3 center, float halfscale)
+        {
+            float quaterscale = halfscale * 0.5f;
+            bool leaf = true;
+            for (int i = 0; i < 8; i++)
+                if (_octree_dump[index + i] >= index + 8)
+                {
+                    leaf = false;
+                    Vector3 delta = quaterscale * Vector3.one;
+                    if ((i & 1) == 0) delta.x = -delta.x;
+                    if ((i & 2) == 0) delta.y = -delta.y;
+                    if ((i & 4) == 0) delta.z = -delta.z;
+                    DrawRec(_octree_dump[index + i], center + delta, quaterscale);
+                }
+            if (leaf)
+            {
+                Gizmos.color = new Color(1, 0.7f, 0.7f);
+                Gizmos.DrawWireCube(center, halfscale * 2 * Vector3.one);
+            }
+            else
+            {
+                /*Gizmos.color = Color.red;
+                Gizmos.DrawWireCube(center, halfscale * 2 * Vector3.one);*/
+            }
+        }
+
+        DrawRec(OCTREE_ROOT, Vector3.zero, 0.5f * (1 << gridLevels) * gridPixelSizeLevel0 * gridResolution);
+    }
+}
---
+++ Assets/OctreeBuilder.shader
@@ -0,0 +1,105 @@
+﻿Shader "Hidden/VCT/OctreeBuilder" {
+    Properties
+    {
+    }
+
+    SubShader {
+        Tags { "RenderType"="Opaque" }
+
+        Pass
+        {
+            ZTest off
+            ZWrite off
+            Cull off
+            ColorMask 0
+
+            CGPROGRAM
+            #pragma target 5.0
+            #include "UnityCG.cginc"
+            #pragma vertex vert
+            #pragma fragment frag
+            #pragma multi_compile   _ ORIENTATION_2 ORIENTATION_3
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+            };
+
+            struct v2f
+            {
+                float4 vertex : SV_POSITION;
+            };
+
+            #define OCTREE_ROOT  8
+            RWStructuredBuffer<int> Octree : register(u1);
+            float4 _VCT_Scale;
+            uint _VCT_TreeLevels;
+
+
+            v2f vert(appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                return o;
+            }
+
+            fixed4 frag(v2f i) : SV_Target
+            {
+                float3 pos = i.vertex.xyz;
+#if defined(UNITY_REVERSED_Z)
+                pos.z = 1.0 - pos.z;
+#endif
+                pos *= _VCT_Scale.xxy;
+                pos += _VCT_Scale.zzz;
+#ifdef ORIENTATION_2
+                pos = pos.yzx;
+#endif
+#ifdef ORIENTATION_3
+                pos = pos.zxy;
+#endif
+                /* now xyz should be in octree coordinates, [0-1]^3 */
+
+                clip(pos);
+                clip(0.999999 - pos);
+
+                int index = OCTREE_ROOT;
+                int next_index;
+                for (uint i = 0; i < _VCT_TreeLevels; i++)
+                {
+                    index += pos.x >= 0.5 ? 1 : 0;
+                    index += pos.y >= 0.5 ? 2 : 0;
+                    index += pos.z >= 0.5 ? 4 : 0;
+
+                    next_index = Octree[index];
+                    if (next_index <= 0)
+                    {
+                        InterlockedCompareExchange(Octree[index], 0, -1, next_index);
+                        if (next_index == 0)
+                        {
+                            next_index = Octree.IncrementCounter() * 8 + 16;
+                            Octree[index] = next_index;
+                        }
+                        else
+                        {
+#if defined(ORIENTATION_2)
+                            Octree[1] = 1;
+#elif defined(ORIENTATION_3)
+                            Octree[2] = 1;
+#else
+                            Octree[0] = 1;
+#endif
+                            return fixed4(0, 0, 0, 0);   /* dummy result, ignored */
+                        }
+                    }
+
+                    index = next_index;
+                    pos = frac(pos * 2.0);
+                }
+
+                /* dummy result, ignored */
+                return fixed4(0, 0, 0, 0);
+            }
+            ENDCG
+        }
+    }
+}
